0aa591b5a09174350417fa3a46c72944c08dbe9c
sriram bala <sr.bala.kkl@gmail.com>
2022-02-11 11:24:45 +0530

    NEW:formEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ObsrevableWithFormEvent';
+    title = 'ObsrevableWithFormEvent';
+
+    constructor() {
+        /*this.fromEvent(document.body, "click").subscribe({
+            next() {
+                console.log('Body is Clicked')
+            }
+        })*/
+
+        this.fromEvent(document.body, "click").subscribe(x => {
+            console.log('body clicked')
+        })
+    }
+
+
+    fromEvent<T extends keyof HTMLElementEventMap>(target: HTMLElement, eventName: T) {
+        return new Observable<HTMLElementEventMap[T]>((observer) => {
+            const handler = (e: HTMLElementEventMap[T]) => observer.next(e);
+
+            // Add the event handler to the target
+            target.addEventListener(eventName, handler);
+
+            return () => {
+                // Detach the event handler from the target
+                //todo: If we don't use below command then there will be a memory leakage
+                target.removeEventListener(eventName, handler);
+            };
+        });
+    }
+
+    unsubscribeMtd() {
+        // this.bodyClick$ = null;
+    }
 }
